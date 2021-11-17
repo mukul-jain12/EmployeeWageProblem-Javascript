20748a430037c7e7537b3444d6f022462a6040c4
Mukul Jain <mukuljain422@gmail.com>
2021-11-17 16:57:53 +0530

    calculating wage till 100 hours reached

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -3,6 +3,7 @@
 const IS_FULL_TIME = 1;
 const IS_PART_TIME = 2;
 const WAGE_PER_HOURS = 20;
+const MAX_HRS_IN_MONTHS = 100;
 const NUM_OF_WORKING_DAYS = 28;
 
 //function return working hours
@@ -18,13 +19,21 @@ function getWorkingHours(empCheck){
 }
 
 let empHrs = 0;
-for (let day = 0; day < NUM_OF_WORKING_DAYS; day++){
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+while (totalEmpHrs <= MAX_HRS_IN_MONTHS && totalWorkingDays < NUM_OF_WORKING_DAYS){
+    totalWorkingDays++;
     //gives random value
     let empCheck = Math.floor(Math.random() * 10) % 3;
     //calling function
-    empHrs += getWorkingHours(empCheck);
+    empHrs = getWorkingHours(empCheck);
+
+    totalEmpHrs += empHrs;
+    if(totalEmpHrs > 100){
+        totalEmpHrs -= empHrs;
+    }
 }
 
 //calculating employee wage
-let empWage = empHrs * WAGE_PER_HOURS;
-console.log("Employee Wage: " + empWage + " and Total Employee Hours: " + empHrs);
+let empWage = totalEmpHrs * WAGE_PER_HOURS;
+console.log("Employee Wage: " + empWage + ", Total Employee Hours: " + totalEmpHrs + " and Total Working Days: " + totalWorkingDays);
