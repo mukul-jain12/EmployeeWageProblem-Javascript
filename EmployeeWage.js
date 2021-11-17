d5d70eeb65b0e1970341e92c346ab89c68347449
Mukul Jain <mukuljain422@gmail.com>
2021-11-17 16:06:46 +0530

    checking attendance

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -2,7 +2,7 @@
 
 const IS_ABSENT = 0;
 
-let empCheck = Math.floor(Math.random() * 10);
+let empCheck = Math.floor(Math.random() * 10) % 2;
 if(empCheck == IS_ABSENT){
     console.log("Employee is Absent");
 }
